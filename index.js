27fcc1d414d181e9bd908dc0fa4620d5950d5ba9
Ali Falahi <ali@afalahi.com>
2021-11-06 00:50:38 -0600

    init

---
+++ index.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const express = require('express');
+const debug = require('debug');
+
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('app running'));
+server.on('error', onError);
+server.on('listening', onListening);
+
+app.get('/', (req, res) => {
+  const headers = req.headers;
+  res.json(headers);
+});
+
+function onError (error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+function normalizePort (val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+function onListening  ()  {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+};
